dcf7660d196bd83d1febefe3b3af4db8bc139b58
Holy <pestofraise@gmail.com>
2021-03-04 10:24:58 +0100

    fusion branches Quentin et Tommy fonctionnelle

---
+++ src/Controller/LogsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LogsController extends AbstractController
+{
+    #[Route('/logs', name: 'logs')]
+    public function index(): Response
+    {
+        return $this->render('logs/index.html.twig', [
+            'controller_name' => 'LogsController',
+        ]);
+    }
+}

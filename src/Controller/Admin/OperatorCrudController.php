63af6d1d0705177ccd23f81a39170f07fdc623d0
Mod_loc <Mod_loc@D46-P03.D-Electronique.local>
2021-03-15 11:23:03 +0100

    ajout interface easyadmin (/admin) + reset du password (/reset-password)

---
+++ src/Controller/Admin/OperatorCrudController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use App\Entity\Operator;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+
+
+class OperatorCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Operator::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            EmailField::new('email'),
+            ArrayField::new('roles'),
+            TextField::new('password'),
+            BooleanField::new('is_verified'),
+        ];
+    }
+    
+}

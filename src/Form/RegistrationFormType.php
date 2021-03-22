9c165d9e209d0af64c36cf433112803044ebf4e6
Holy <pestofraise@gmail.com>
2021-03-22 11:27:36 +0100

    ajout de la vérification du mail dans le formulaire

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,13 +11,14 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+        ->add('email', EmailType::class)
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller

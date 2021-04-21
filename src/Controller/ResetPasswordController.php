8996367de180113ef135f5240c7e366aa78aeb3f
engooref <55575789+engooref@users.noreply.github.com>
2021-04-21 09:43:33 +0200

    Prod

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Operateur;
 use Symfony\Component\HttpFoundation\Request;
-use App\Form\ResetPasswordFormType;
+use App\Form\ResetPasswordType;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Repository\OperateurRepository;
 
@@ -17,7 +17,7 @@ class ResetPasswordController extends AbstractController
     public function index($token, Request $request, OperateurRepository $userRepo, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = $userRepo->findOneBy(['reset_token'=>$token]);
-        $form = $this->createForm(ResetPasswordFormType::class);
+        $form = $this->createForm(ResetPasswordType::class);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password

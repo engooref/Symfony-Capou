c08c7b9312df1e835411e92373087c24eec1ae74
Yoan <guiraudyoan@outlook.fr>
2021-05-20 15:56:45 +0200

    Création de compte par l'administrateur fonctionnel
    Génération du mot de passe aléatoire (8 caractères)
    Cryptage du mot de passe dans la BDD 
    Envoie mail à l'utilisateur (id + mdp)

--- src/Controller/Admin/OperateurCrudController.php
+++ src/Controller/Admin/OperateurCrudController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Groupe;
 use App\Entity\Operateur;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class OperateurCrudController extends AbstractCrudController
@@ -19,8 +24,16 @@ class OperateurCrudController extends AbstractCrudController
     {
         return [
             IdField::new('id')->hideOnForm(),
-            TextField::new('email'), 
-            
+            EmailField::new('email')->setHelp('Choisir l\'adresse email'), 
+            AssociationField::new('idGroupe', 'Groupe')->setRequired(true)->setHelp('Choisir à quel groupe l\'opérateur appartient'),
+            DateTimeField::new('createdAt')->hideOnForm(),
         ];
     }
+    
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setDefaultSort(['createdAt' => 'DESC']);
+    }
+    
 }

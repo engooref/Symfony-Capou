b305d44e8946bc71b3d2ba41997fc2c6cccedff8
engooref <armand2001@live.fr>
2021-07-05 01:17:09 +0200

    Correction

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -25,53 +25,75 @@ class HomeController extends AbstractController
 
         //Piquet
         $nbpiquet_print = 10;
-        $nb_piquet = count($this->getDoctrine()->getManager()->getRepository(Piquet::class)->findByEtat(1));
-        if ($nb_piquet < $nbpiquet_print) $nbpiquet_print = $nb_piquet;
-        for($k = 0; $k<$nbpiquet_print; $k++){
-            $donnees_piquet[$k] = $this->getDoctrine()->getManager()->getRepository(DonneesPiquet::class)->findOneByIdPiquet([$k+1],['horodatage' => 'desc']);
-            $parcellePiquet[$k] = $this->getDoctrine()->getManager()->getRepository(Piquet::class)->findOneById($k+1)->getIdParcelle();
+        $piquetParse = 0;
+        $piquetDb = $this->getDoctrine()->getManager()->getRepository(Piquet::class)->findByEtat(1);
+        for($piquetParse = 0; ($piquetParse < $nbpiquet_print) && ($piquetParse < count($piquetDb)); $piquetParse++){
+            $donnees_piquet[$piquetParse] = $this->_sortArray($piquetDb[$piquetParse]->getIdDonnees()->getValues())[0];
+            $parcellePiquet[$piquetParse] = $piquetDb[$piquetParse]->getIdParcelle();
         }
-        
+      
         //Armoire
-        $etat_armoire = $this->getDoctrine()->getManager()->getRepository(Armoire::class)->findAll();
-        $etat_armoire = $etat_armoire[0]->getEtat();
-        $donnees_armoire = $this->getDoctrine()->getManager()->getRepository(DonneesArmoire::class)->findOneBy([],[]);
+        $armoireDb = $this->getDoctrine()->getManager()->getRepository(Armoire::class)->findAll()[0];
+        $etat_armoire = $armoireDb->getEtat();
+        $donnees_armoire = $armoireDb->getIdDonnees()->getValues()[0];
 
         //Electrovanne
         $nbvanne_print = 5;
-        $nb_vanne = count($this->getDoctrine()->getManager()->getRepository(ElectroVanne::class)->findByEtat(1));
-        if ($nb_vanne < $nbvanne_print) $nbvanne_print = $nb_vanne;
-        for($k = 0; $k<$nbvanne_print; $k++){
-            $donnees_vanne[$k] = $this->getDoctrine()->getManager()->getRepository(DonneesVanne::class)->findOneByIdVanne([$k+1],['horodatage' => 'desc']);
-            $parcelleVanne[$k] = $this->getDoctrine()->getManager()->getRepository(ElectroVanne::class)->findOneById($k+1)->getIdParcelle();
-            
+        $vanneParse = 0;
+        $vanneDb = $this->getDoctrine()->getManager()->getRepository(ElectroVanne::class)->findAll();
+        for($vanneParse = 0; ($vanneParse < $nbvanne_print) && ($vanneParse < count($vanneDb)); $vanneParse++){
+            $donnees_vanne[$vanneParse] = $this->_sortArray($vanneDb[$vanneParse]->getIdDonnees()->getValues())[0];
+            $parcelleVanne[$vanneParse] = $vanneDb[$vanneParse]->getIdParcelle();
         }
+
         //Parcelle
-        $nb_parcelle = count($this->getDoctrine()->getManager()->getRepository(Parcelle::class)->findAll());
-        for($k = 0; $k<$nb_parcelle; $k++){
-           $parcelle[$k] = $this->getDoctrine()->getManager()->getRepository(Parcelle::class)->findOneById($k+1)->getLabel();
-           $pPiquet[$k] = count($this->getDoctrine()->getManager()->getRepository(Piquet::class)->findByIdParcelle($k+1));
-           $pVanne[$k] = count($this->getDoctrine()->getManager()->getRepository(ElectroVanne::class)->findByIdParcelle($k+1));         
-           $pOperateur[$k] = count($this->getDoctrine()->getManager()->getRepository(Operateur::class)->findByIdParcelle($k+1));
-        }
+        $parcelleDb = $this->getDoctrine()->getManager()->getRepository(Parcelle::class)->findAll();
+        $parcelleParse = 0;
+        foreach($parcelleDb as $parcelle) {
+            $parcelleParse = array_search($parcelle, $parcelleDb);
+            $labelParcelle[$parcelleParse] = $parcelle->getLabel();
+            $pPiquet[$parcelleParse] = count($parcelle->getIdPiquets()->getValues());
+            $pVanne[$parcelleParse] = count($parcelle->getIdElectrovannes()->getValues());
+            $pOperateur[$parcelleParse] = count($parcelle->getIdOperateurs()->getValues());
         
+        }
+      
         return $this->render('home/index.html.twig', [
-            'nb_piquet' => $nb_piquet,
-            'nbpiquet_print' => $nbpiquet_print,
+            'nb_piquet' => $piquetParse,
             'parcellePiquet' => $parcellePiquet,
             'donnees_piquet' => $donnees_piquet,
-            'nb_vanne' => $nb_vanne,
-            'nbvanne_print' => $nbvanne_print,
-            'parcelleVanne' => $parcelleVanne,
-            'donnees_vanne' => $donnees_vanne,
+
             'etat_armoire' => $etat_armoire,
             'donnees_armoire' => $donnees_armoire,
-            'nb_parcelle' => $nb_parcelle,
-            'donnees_parcelle' => $parcelle,
+
+            'nb_vanne' => $vanneParse,
+            'parcelleVanne' => $parcelleVanne,
+            'donnees_vanne' => $donnees_vanne,
+
+            'nb_parcelle' => $parcelleParse,
+            'donnees_parcelle' => $labelParcelle,
             'nbParcelle_piquet' => $pPiquet,
             'nbParcelle_vanne' => $pVanne,
             'nbParcelle_operateur' => $pOperateur,
         ]);
         
+    }
+
+    private function _sortArray(Array $fields) : Array 
+    {
+        $sortArray = array();
+        for($index = 0; $index < count($fields); $index++){    
+            $max = $fields[$index];
+            for($parcelleParse = $index; ($parcelleParse < count($fields)); $parcelleParse++){
+                if($max->getHorodatage() < $fields[$parcelleParse]->getHorodatage()){
+                    $max = $fields[$parcelleParse];
+                }
+            }
+            $fields[array_search($max, $fields)] = $fields[$index];
+            $fields[$index] = $max;
+        }
+        
+        $sortArray = $fields;
+        return $sortArray;
     }
 }
